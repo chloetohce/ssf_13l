548014a573292c5a207d1972d82ade182eeda1db
chloetohce <chloetohce@gmail.com>
2024-11-20 10:49:19 +0800

    Spring Project

---
+++ src/main/java/sg/edu/nus/iss/ssf_13l/Ssf13lApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssf_13l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssf13lApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssf13lApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/ssf_13l/Ssf13lApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssf_13l;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssf13lApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
